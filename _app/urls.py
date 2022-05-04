d6b43e40f8feffdd3480ddbfe0e317249ae61f31
barusasu <barusasu11209@gmail.com>
2022-05-04 10:26:29 +0900

    ログイン機能の実装

--- _app/urls.py
+++ _app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index_func, name='index'),
+    path('signup/', views.signupfunc, name='signup'),
+    path('login/', views.loginfunc, name="login"),
+    path('logout/', views.logoutfunc, name='logout'),
 ]
--- _app/views.py
+++ _app/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'login.html', {})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーはすでに登録されています'})
+    
+    return render(request, 'sigup.html', {})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {})
+
+    return render(request, 'login.html', {})
+
+@login_required
 def index_func(request):
     return render(request, 'index.html', {})
+
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
